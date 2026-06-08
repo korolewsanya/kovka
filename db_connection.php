e1065d8600063282a0f988e3a6acb20af3230b3e
korolewsanya <korolewsanya@gmail.com>
2026-06-08 15:21:04 +0300

    no message

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,9 @@
 <?php
-// Защита от прямого доступа к файлу 
-defined('ALLOWED') or die('Direct access not allowed');
+
 $conn = new mysqli("localhost", "root", "", "kovka");
 
-// Проверка подключения
 if ($conn->connect_error) {
-    // Логируем ошибку вместо вывода на экран (безопаснее)
-    error_log("Database connection failed: " . $conn->connect_error);
-    die("Извините, технические неполадки. Попробуйте позже.");
+    die("Ошибка подключения: " . $conn->connect_error);
 }
-
 // Установка кодировки
 $conn->set_charset("utf8"); 
