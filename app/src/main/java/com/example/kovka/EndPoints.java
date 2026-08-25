ab29c794641b7c759bce6e134efe55493f00f9df
korolewsanya <korolewsanya@gmail.com>
2026-08-25 21:20:17 +0300

    Merge remote changes

--- app/src/main/java/com/example/kovka/EndPoints.java
+++
@@ -1,8 +0,0 @@
-package com.example.kovka;
-
-public class EndPoints {
-
-    private static final String ROOT_URL = Config.API_BASE + "upload_image.php?apicall=";
-
-    public static final String UPLOAD_URL = ROOT_URL + "uploadpic";
-}
